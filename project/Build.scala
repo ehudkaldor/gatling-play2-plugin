50abc12c4594d7b26df83869bbe2523a3d9ca352
Andy Petrella <andy.petrella@gmail.com>
2012-04-20 14:42:00 +0200

    Switch to Gatling 1.1.4-SNAPSHOT thanks to Stephane (this version has to be built locally using maven)

--- project/Build.scala
+++ project/Build.scala
@@ -13,15 +13,17 @@ object MinimalBuild extends Build {
   val gatlingReleasesRepo = "Gatling Releases Repo" at "http://repository.excilys.com/content/repositories/releases"
   val gatling3PartyRepo = "Gatling Third-Party Repo" at "http://repository.excilys.com/content/repositories/thirdparty"
 
+  /* LOCAL MAVEN REPO */
+  val localMavenRepo = "Local Maven Repository" at file(Path.userHome.absolutePath+"/.m2/repository").toURI.toURL.toString
 
   /* GATLING DEPS */
-  val gatlingVersionNumber = "1.1.3"
-  val gatlingApp = "com.excilys.ebi.gatling" % "gatling-app" % gatlingVersionNumber withSources
-  val gatlingCore = "com.excilys.ebi.gatling" % "gatling-core" % gatlingVersionNumber withSources
-  val gatlingHttp = "com.excilys.ebi.gatling" % "gatling-http" % gatlingVersionNumber withSources
-  val gatlingRecorder = "com.excilys.ebi.gatling" % "gatling-recorder" % gatlingVersionNumber withSources
-  val gatlingCharts = "com.excilys.ebi.gatling" % "gatling-charts" % gatlingVersionNumber withSources
-  val gatlingHighcharts = "com.excilys.ebi.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersionNumber withSources
+  val gatlingVersionNumber = "1.1.4-SNAPSHOT"
+  val gatlingApp = "com.excilys.ebi.gatling" % "gatling-app" % gatlingVersionNumber //withSources
+  val gatlingCore = "com.excilys.ebi.gatling" % "gatling-core" % gatlingVersionNumber //withSources
+  val gatlingHttp = "com.excilys.ebi.gatling" % "gatling-http" % gatlingVersionNumber //withSources
+  val gatlingRecorder = "com.excilys.ebi.gatling" % "gatling-recorder" % gatlingVersionNumber //withSources
+  val gatlingCharts = "com.excilys.ebi.gatling" % "gatling-charts" % gatlingVersionNumber //withSources
+  val gatlingHighcharts = "com.excilys.ebi.gatling.highcharts" % "gatling-charts-highcharts" % gatlingVersionNumber //withSources
 
 
   val specs2 = "org.specs2" %% "specs2" % "1.8.2" withSources
@@ -46,7 +48,7 @@ object MinimalBuild extends Build {
   lazy val root = Project(id = appName, base = file("."), settings = Project.defaultSettings).settings(
     version := buildVersion,
     organization := "be.nextlab",
-    resolvers ++= Seq(typesafe, typesafeSnapshot, gatlingReleasesRepo, gatling3PartyRepo),
+    resolvers ++= Seq(typesafe, typesafeSnapshot, gatlingReleasesRepo, gatling3PartyRepo, localMavenRepo),
     javacOptions += "-Xlint:unchecked",
     libraryDependencies ++= libDependencies
   )
--- src/main/scala/be/nextlab/play/gatling/Util.scala
+++ src/main/scala/be/nextlab/play/gatling/Util.scala
@@ -7,20 +7,8 @@ import org.joda.time.DateTime._
 import com.excilys.ebi.gatling.core.runner.Runner
 import org.specs2.specification.Around
 import org.specs2.execute.Result
-import play.api.test.Helpers._
 import play.api.test.{TestServer, FakeApplication}
-import com.excilys.ebi.gatling.core.result.writer.DataWriter
-import akka.util.Duration._
-import com.excilys.ebi.gatling.core.config.GatlingConfiguration._
-import java.util.concurrent.TimeUnit._
-import akka.util.Duration
-import akka.util.duration.longToDurationLong
-import com.excilys.ebi.gatling.core.action.system
-import com.excilys.ebi.gatling.core.scenario.configuration.{ScenarioConfiguration, ScenarioConfigurationBuilder, Simulation}
-import akka.actor.ActorRef
-import com.excilys.ebi.gatling.core.session.Session
-import com.excilys.ebi.gatling.app.Gatling._
-import com.excilys.ebi.gatling.core.resource.ResourceRegistry
+import com.excilys.ebi.gatling.core.scenario.configuration.Simulation
 
 /**
  *
@@ -34,9 +22,9 @@ object Util {
   lazy val fakeApplication: FakeApplication = FakeApplication(additionalPlugins = Seq("be.nextlab.play.gatling.GatlingPlugin"))
   lazy val plugin: GatlingPlugin = fakeApplication.plugin[GatlingPlugin].get
 
-  def createServer(port:Int, application:FakeApplication = fakeApplication) = new TestServer(port, application)
+  def createServer(port: Int, application: FakeApplication = fakeApplication) = new TestServer(port, application)
 
-  case class GatlingApp(s:Simulation) extends Around {
+  case class GatlingApp(s: Simulation) extends Around {
 
     def around[T <% Result](t: => T) = {
       gatling(s)
@@ -44,23 +32,19 @@ object Util {
     }
 
     def gatling(s: Simulation) {
-      //useActorSystem {
-        println("Creating run record")
-        val runInfo = new RunRecord(now, "run-test", "test" + System.currentTimeMillis)
-        println("Run record created > run scenario")
+      println("Creating run record")
+      val runInfo = new RunRecord(now, "run-test", "stress-test")
+      println("Run record created > run scenario")
 
-        val configurations = s()
-        new Runner(runInfo, configurations).run
+      val configurations = s()
+      new Runner(runInfo, configurations).run
 
-        println("Simulation Finished.")
-        runInfo.runUuid
+      println("Simulation Finished.")
+      runInfo.runUuid
 
-        println("scenarion ran > generate reports")
-        generateReports(runInfo.runUuid)
-        println("reports generated")
-
-        //ResourceRegistry.closeAll
-      //}
+      println("scenarion ran > generate reports")
+      generateReports(runInfo.runUuid)
+      println("reports generated")
     }
 
     def generateReports(runUuid: String) {
